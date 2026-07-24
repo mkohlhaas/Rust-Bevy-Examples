9edd92f7116c34a8b8a4e55bfa3b1e5c3f2b47bb
Hans-J. Schmid <knock@myopendoor.de>
2026-07-24 10:19:24 +0200

    remove github crap

--- .github/start-mobile-example/specs/screenshot.js
+++
@@ -1,9 +0,0 @@
-describe("Running Bevy Example", () => {
-  it("can take a screenshot", async () => {
-    // Sleep to wait for app startup, device rotation, ...
-    await new Promise((r) => setTimeout(r, 5000));
-
-    // Take local screenshot
-    await browser.saveScreenshot("./screenshot.png");
-  });
-});
